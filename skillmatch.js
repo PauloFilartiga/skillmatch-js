b3b6c6e816a33782f7a0b3ed2a1d6ee3155b1b48
Filartiga <106940710+PauloFilartiga@users.noreply.github.com>
2026-07-16 13:50:16 -0400

    feat: adiciona perfil do candidato

---
+++ skillmatch.js
@@ -0,0 +1,10 @@
+const candidato = {
+    nome: "Paulo",
+    area: "Front-End",
+    habilidades: ["JavaScript", "GitHub", "Lógica de Programação", "Kanban"
+    ],
+    experienciaMeses: 3
+};
+
+console.log("Perfil do candidato:");
+console.log(candidato);
