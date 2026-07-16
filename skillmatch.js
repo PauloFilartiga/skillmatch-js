fa54c9ef046d58e309b3483b588e4d1b9563ac8e
Filartiga <106940710+PauloFilartiga@users.noreply.github.com>
2026-07-16 15:06:39 -0400

    feat: adiciona lista de vagas

--- skillmatch.js
+++ skillmatch.js
@@ -8,3 +8,46 @@ const candidato = {
 
 console.log("Perfil do candidato:");
 console.log(candidato);
+
+const vagas = [
+    {
+     id: 1,
+     empresa: "TechStart",
+     cargo: "Desenvolvedor Front-End Júnior",
+     requisitos: [
+       "JavaScript",
+       "GitHub",
+       "Lógica de Programação"
+     ],
+     salario: 2800,
+     modalidade: "Remoto"
+    },
+    {
+     id: 2,
+     empresa: "CodeLab",
+     cargo: "Estágio Front-End",
+     requisitos: [
+       "JavaScript",
+       "Kanban",
+       "GitHub"
+     ],
+     salario: 1800,
+     modalidade: "Híbrido" 
+    },
+    {
+     id: 3,
+     empresa: "WebSolutions",
+     cargo: "Programador JavaScript Júnior",
+     requisitos: [
+        "JavaScript",
+        "Arrays",
+        "Objetos",
+        "Funções"
+     ],
+     salario: 3000,
+     modalidade: "Presencial"
+    },
+];
+
+console.log("\nLista de vagas:");
+console.log(vagas);
